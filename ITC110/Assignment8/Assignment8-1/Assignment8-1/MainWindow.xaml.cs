50fcb0d0ea1cbe15c0bab8047ad460d576d3fec6
Kyrrah Nork <kyrrahnork@gmail.com>
2016-11-22 11:46:38 -0800

    Add files via upload

---
+++ ITC110/Assignment8/Assignment8-1/Assignment8-1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment8_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Total t = new Assignment8_1.Total();
+            t.MealPrice = double.Parse(textBoxMealPrice.Text);
+
+           
+            if(checkBox10.IsChecked == true)
+            {
+                t.Tip = .1;
+            }
+            else if(checkBox15.IsChecked == true)
+            {
+                t.Tip = .15;
+            }
+            else if(checkBox20.IsChecked == true)
+            {
+                t.Tip = .2;
+            }
+            else
+            {
+                t.Tip = double.Parse(textBoxOther.Text);
+                if(t.Tip > 1)
+                {
+                    t.Tip = t.Tip / 100;
+                }
+            }
+            double grandTotal = t.TotalBill();
+
+            labelTotal.Content = "Your total bill, including tax and tip is: $" + t.TotalBill().ToString("0.00");
+        }
+    }
+}
---
+++ ITC110/Assignment8/Assignment8-1/Assignment8-1/Total.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8_1
+{
+    public class Total
+    {
+        private double mealPrice;
+        private double tip;
+
+        public Total()
+        {
+            MealPrice = 0;
+            Tip = 0;
+        }
+
+        public Total(double totalMealPrice, double totalTip)
+        {
+            MealPrice = totalMealPrice;
+            Tip = totalTip;     
+        }
+
+        public double MealPrice
+        {
+            get
+            {
+                return mealPrice;
+            }
+            set
+            {
+                mealPrice = value;
+            }
+        }
+
+        public double Tip
+        {
+            get
+            {
+                return tip;
+            }
+            set
+            {
+                tip = value;
+            }
+        }
+
+        public double GetTax()
+        {
+            double tax = .09;
+            return MealPrice * tax;
+        }
+
+        public double BillTip()
+        {
+            return (GetTax() + MealPrice) * Tip;
+        }
+
+        public double TotalBill()
+        {
+            return GetTax() + BillTip() + MealPrice;
+        }
+    }
+}
